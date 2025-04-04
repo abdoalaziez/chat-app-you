72f7688babf90152ebdf42b910fd560bff75786c
umran <umranabdo112@gmail.com>
2025-04-04 15:25:33 +0200

    chat app you

---
+++ server.js
@@ -0,0 +1,35 @@
+import app from "./app.js";
+import cloudinary from "cloudinary";
+
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello Umran!");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening at port ${process.env.PORT}`);
+});
+
+
+
+// import app from "./app.js";
+// import cloudinary from "cloudinary";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// cloudinary.v2.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server listening at port ${PORT}`);
+// });
